2a0fe64b58343ea44b7872c3c70bf4a69a906744
Eren Dinç <iamerndnc@gmail.com>
2024-01-01 21:55:48 +0300

    Web Programlama Proje Final Versiyonu

--- AirplaneSeatReservation/AirplaneSeatReservation/AirplaneSeatReservation/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AirplaneSeatReservation.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
--- AirplaneSeatReservation/AirplaneSeatReservation/Program.cs
+++ AirplaneSeatReservation/AirplaneSeatReservation/Program.cs
@@ -1,15 +1,57 @@
 using AirplaneSeatReservation.Models;
+using AirplaneSeatReservation.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using ServiceStack;
+using System.Globalization;
+using System.Reflection;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+#region Localizer
+builder.Services.AddSingleton<LanguageService>();
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+builder.Services.AddMvc().AddViewLocalization().AddDataAnnotationsLocalization(options =>
+options.DataAnnotationLocalizerProvider = (Type, factory) =>
+{
+	var assemblyName = new AssemblyName(typeof(SharedResource).GetTypeInfo().Assembly.FullName);
+	return factory.Create(nameof(SharedResource), assemblyName.Name);
+});
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+	var supportCultures = new List<CultureInfo>
+	{
+		new CultureInfo("tr-TR"),
+		new CultureInfo("en-US"),
+    };
+	options.DefaultRequestCulture = new RequestCulture(culture: "tr-TR", uiCulture: "tr-TR");
+	options.SupportedCultures = supportCultures;
+	options.SupportedUICultures = supportCultures;
+	options.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+});
+#endregion
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+	options.Cookie.Name = "NetCoreMvc.Auth";
+	options.LoginPath = "/User/Login";
+	options.AccessDeniedPath = "/User/Login";
+});
+
+builder.Services.AddAuthorization(options =>
+{
+	options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+	options.AddPolicy("RequireUiRole", policy => policy.RequireRole("Ui"));
+});
 builder.Services.AddDbContext<FlightCS>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddSession();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -17,10 +59,10 @@ if (!app.Environment.IsDevelopment())
 	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 	app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseRequestLocalization(app.Services.GetRequiredService<Microsoft.Extensions.Options.IOptions<RequestLocalizationOptions>>().Value);
 app.UseRouting();
 
 app.UseAuthentication();
@@ -30,6 +72,5 @@ app.UseSession();
 
 app.MapControllerRoute(
 	name: "default",
-	pattern: "{controller=Home}/{action=Index}/{id?}");
-
+	pattern: "{controller=Main}/{action=Homepage}/{id?}");
 app.Run();
